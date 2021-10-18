f2d4e6604edc58a4dd99b8d39f8d6b3d0a7100c2
Alexis Schotte <amschotte@me.com>
2021-10-18 16:10:00 +0200

    Initial Commit

---
+++ SwitfulThinkingContinuedLearning/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwitfulThinkingContinuedLearning
+//
+//  Created by Alexis Schotte on 10/18/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwitfulThinkingContinuedLearning/SwitfulThinkingContinuedLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwitfulThinkingContinuedLearningApp.swift
+//  SwitfulThinkingContinuedLearning
+//
+//  Created by Alexis Schotte on 10/18/21.
+//
+
+import SwiftUI
+
+@main
+struct SwitfulThinkingContinuedLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
